8f5d5078d4c07cef5664d5912ae5624f92c17876
luis <luis@correo.com>
2022-04-18 21:25:02 -0500

    Cuarto Commit Rama Arias sprint-arias
    Agregando Constantes y Metodo Insertar con MSJ Error

--- src/main/java/com/example/utils/Constantes.java
+++ src/main/java/com/example/utils/Constantes.java
@@ -2,8 +2,10 @@ package com.example.utils;
 
 public class Constantes {
 
-	public Constantes() {
-		// TODO Auto-generated constructor stub
-	}
+	public static final String MENSAJE_DEPARTAMENTO_YAEXISTE = "Ya existe dicho Departamento.";
+    public static final String MENSAJE_DEPARTAMENTO_REGISTRADO = "Se Registro Departamento Existosamente.";
+    public static final String MENSAJE_DEPARTAMENTO_ACTUALIZADO = "Se Actualizo Departamento Existosamente.";
+    public static final String MENSAJE_DEPARTAMENTO_ELIMINADO = "Se Elimino Departamento Exitosamente.";
+    public static final String MENSAJE_DEPARTAMENTO_ERROR_REGISTRAR = "No se Registro Departamento, Verifique los datos.";
 
 }
