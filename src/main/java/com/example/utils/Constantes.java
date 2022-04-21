0b9ad679d97565b386e25e0178d5ce0fcca61e87
Impalatore <naim7710@gmail.com>
2022-04-20 19:59:58 -0500

    Mascota | Commit HURTADO

--- src/main/java/com/example/utils/Constantes.java
+++ src/main/java/com/example/utils/Constantes.java
@@ -2,8 +2,26 @@ package com.example.utils;
 
 public class Constantes {
 
-	public Constantes() {
-		// TODO Auto-generated constructor stub
-	}
+
+	public static final String MENSAJE_PROPIETARIO_YAEXISTE = "Ya existe dicho Propietario.";
+	public static final String MENSAJE_PROPIETARIO_YAEXISTEDNI = "Ya existe dicho DNI de Propietario.";
+	public static final String MENSAJE_PROPIETARIO_REGISTRADO = "Se Registro al Propietario Existosamente.";
+	public static final String MENSAJE_PROPIETARIO_ACTUALIZADO = "Se Actualizo al Propietario Existosamente.";
+	public static final String MENSAJE_PROPIETARIO_ELIMINADO = "Se Elimino al Propietario Exitosamente.";
+	public static final String MENSAJE_PROPIETARIO_ERROR_REGISTRAR = "No se Registro al Propietario, Verifique los datos.";
+
+
+	public static final String MENSAJE_DEPARTAMENTO_YAEXISTE = "Ya existe dicho Departamento.";
+    public static final String MENSAJE_DEPARTAMENTO_REGISTRADO = "Se Registro Departamento Existosamente.";
+    public static final String MENSAJE_DEPARTAMENTO_ACTUALIZADO = "Se Actualizo Departamento Existosamente.";
+    public static final String MENSAJE_DEPARTAMENTO_ELIMINADO = "Se Elimino Departamento Exitosamente.";
+    public static final String MENSAJE_DEPARTAMENTO_ERROR_REGISTRAR = "No se Registro Departamento, Verifique los datos.";
+    
+	public static final String MENSAJE_MASCOTA_YAEXISTE = "Ya existe dicha Mascota.";
+    public static final String MENSAJE_MASCOTA_REGISTRADO = "Se Registró Mascota Existosamente.";
+    public static final String MENSAJE_MASCOTA_ACTUALIZADO = "Se Actualizó Mascota Existosamente.";
+    public static final String MENSAJE_MASCOTA_ELIMINADO = "Se Eliminó Mascota Exitosamente.";
+    public static final String MENSAJE_MASCOTA_ERROR_REGISTRAR = "No se Registró Mascota, Verifique los datos.";
+
 
 }
