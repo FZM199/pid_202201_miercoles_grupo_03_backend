12e4a5837535bf674d5b6dd4bbb3b22bffddab83
Impalatore <naim7710@gmail.com>
2022-04-27 11:59:51 -0500

    Correcciones en Usuario y Ubigeo | Login Funcional

--- src/main/java/com/example/entity/Ubigeo.java
+++ src/main/java/com/example/entity/Ubigeo.java
@@ -1,51 +1,31 @@
 package com.example.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-/**
- * @author fezam
- *
- */
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
 @Entity
-@Table(name = "Ubigeo")
+@Table(name = "ubigeo")
 public class Ubigeo {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int codubigeo;
+	@Column(name="idUbigeo")
+	private int idUbigeo;
+	@Column(name="departamento")
 	private String departamento;
+	@Column(name="provincia")
 	private String provincia;
+	@Column(name="distrito")
 	private String distrito;
-	
-	public int getCodubigeo() {
-		return codubigeo;
-	}
-	public void setCodubigeo(int codubigeo) {
-		this.codubigeo = codubigeo;
-	}
-	public String getDepartamento() {
-		return departamento;
-	}
-	public void setDepartamento(String departamento) {
-		this.departamento = departamento;
-	}
-	public String getProvincia() {
-		return provincia;
-	}
-	public void setProvincia(String provincia) {
-		this.provincia = provincia;
-	}
-	public String getDistrito() {
-		return distrito;
-	}
-	public void setDistrito(String distrito) {
-		this.distrito = distrito;
-	}
-	
-	
-	
+
 }
--- src/main/java/com/example/entity/Usuario.java
+++ src/main/java/com/example/entity/Usuario.java
@@ -1,10 +1,8 @@
 package com.example.entity;
 
-import java.util.Date;
-
 
 import java.util.Date;
-import javax.persistence.Basic;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -17,7 +15,7 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.transaction.Transactional;
-import org.springframework.format.annotation.DateTimeFormat;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -25,71 +23,48 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-
+@Getter
+@Setter
 @Entity
 @Table(name="Usuario")
+@ToString
 public class Usuario {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Basic(optional = false)
 	@Column(name="idUsuario")
-	private int idUsuario;
-	
-	@Basic(optional = false)
-	@Column(length = 8)
-	private String dni;
-	
-	@Basic(optional = false)
-	@Column(length = 200)
-	private String nombre;
-	
-	@Basic(optional = false)
-	@Column(length = 200)
-	private String primerapellido;
-	
-	@Basic(optional = false)
-	@Column(length = 200)
-	private String segundoapellido;
-	
-	@Basic(optional = false)
-	private String telefono;
-	
-	@Basic(optional = false)
-	private String celular;
-	
-	@Basic(optional = false)
-	@Column(length = 1000)
-	private String direccion;
-	
-	@Basic(optional = false)
-	@Column(length = 200)
-	private String correo;
-	
-	@Basic(optional = false)
-	@Column(length = 10000)
-	private String password;
-	
-	@JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd", timezone="America/Lima")
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private int idUsuario;                //    int                          primary key,
+	@Column(name="dni")
+ 	private String dni;                 //       char(8)       not null       unique,
+	@Column(name="nombre")
+	private String nombre;             //        varchar(30)   not null,
+	@Column(name="correo")
+	private String correo;              //       varchar(50)   not null,
+	@Column(name="login")
+	private String login;
+	@Column(name="password")
+	private String password;          //       char(8)       not null,
+	@Column(name="primerapellido")
+	private String primerapellido;     //        varchar(30)   not null,
+	@Column(name="segundoapellido")
+	private String segundoapellido;    //        varchar(30)   not null,
+	@Column(name="telefono")
+	private String telefono;            //       char(7)       not null,
+	@Column(name="celular")
+	private String celular;            //        char(9)       not null,
+	@Column(name="direccion")
+	private String direccion;          //        varchar(1000) not null,
+	@Column(name="estado")
+	private int    estado;              //  int           not null,
 	@Temporal(TemporalType.TIMESTAMP)
-	@Basic(optional = false)
-	private Date fechareg;
-	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
+	private Date   fechareg;            // date          not null,
 	@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "idubigeo")
-	@Basic(optional = false)
-	private Ubigeo idUbigeo;
-	
-	@Basic(optional = false)
-	private String login;
-	
-	@Basic(optional = false)
-	private boolean estado;
-  @Transactional
+	@JoinColumn(name = "idUbigeo")
+	private Ubigeo   idUbigeo;          
+    @Transactional
 	public String getNombreCompleto() {
 		return nombre.concat(" ").concat(primerapellido+segundoapellido);
 	}
 	
 }
-	
